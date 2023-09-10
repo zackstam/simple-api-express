6adbd216dd1260209fec0bebb8378039929f0b5a
muhammad zakuan <zackstam@gmail.com>
2023-09-10 13:30:28 +0700

    add route

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,8 @@ const router = express.Router();
 const user = require('./user');
 
 router.use('/user', user);
+router.use('/', (req, res, next) => {
+    return res.status(200).json({ message: 'success', data: 'Hello world' })
+});
+
 module.exports = router;
