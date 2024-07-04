335e551e7f6a9d8f3d52f956c272109faae3242e
zakuan <zackstam@gmail.com>
2024-07-04 07:27:21 +0700

    handle for app engine

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,6 @@ RUN npm install
 
 COPY . /app
 
-CMD ["node", "app.js"]
+CMD ["node", "index.js"]
 
 EXPOSE 3000
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker build -t sample-app-api .
-docker run -d -p 3000:3000 sample-app-api
+docker run -d -p 8080:3000 sample-app-api
 docker ps -a
